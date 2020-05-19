15d000040f8fb5670cee4463c209b69fb92bbbf7
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-05-19 11:42:13 +0200

    upgrading sampels 11

--- resources/demo/onboard_carts_qualitygates.sh
+++ resources/demo/onboard_carts_qualitygates.sh
@@ -2,6 +2,10 @@
 
 #If directory exists continue, otherwise exit
 if [[ -d "carts" ]]; then
+
+    # TODO Set up SLI here?
+    # kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/0.4.1/deploy/service.yaml
+
     # The context for this script needs to be in examples/onboarding-carts
     keptn add-resource --project=sockshop --stage=staging --service=carts --resource=slo-quality-gates.yaml --resourceUri=slo.yaml
 
