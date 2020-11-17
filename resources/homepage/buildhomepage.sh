ae868942246472852c2277ee44da4e115f618bfd
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-11-17 18:32:12 +0100

    git in homepage shinojosa/nginxacm:0.7.3

--- resources/homepage/buildhomepage.sh
+++ resources/homepage/buildhomepage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # we build the image and tag it
-docker build -t shinojosa/nginxacm:0.7 .
+docker build -t shinojosa/nginxacm:0.7.3 .
 
 # Pushit to dockerhub
-docker push shinojosa/nginxacm:0.7
+docker push shinojosa/nginxacm:0.7.3
