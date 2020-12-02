16226c62efb8b431ce0c0d796f36aa088e7ae43b
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-02 16:04:21 -0500

    Update onboard_catalog.sh

--- resources/catalog/onboard_catalog.sh
+++ resources/catalog/onboard_catalog.sh
@@ -10,8 +10,8 @@ if [[ -d "catalog" ]]; then
     keptn onboard service front-end --project=catalog --chart=./frontend
     keptn onboard service order-service --project=catalog --chart=./order
     
-    keptn add-resource --project=catalog --stage=dev --service=front-end --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
-    keptn add-resource --project=catalog --stage=staging --service=front-end --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
+    #keptn add-resource --project=catalog --stage=dev --service=front-end --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=catalog --stage=staging --service=front-end --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 
 else 
     echo "The helmcharts for catalog are not present"
