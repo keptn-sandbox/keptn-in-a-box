3bba6e10f392c7a1e10a7f88e7bc8f88c1d53f88
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-02 13:38:44 -0500

    Update onboard_catalog.sh

--- resources/catalog/onboard_catalog.sh
+++ resources/catalog/onboard_catalog.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -x
 
 #If directory exists continue, otherwise exit
-#if [[ -d "k8" ]]; then
+if [[ -d "catalog" ]]; then
     # The context for this script needs to be in examples/onboarding-carts
     keptn create project catalog --shipyard=./shipyard.yaml
     # Onboarding - prepare  Keptn
-    #keptn onboard service catalog --project=catalog --chart=./carts
-    
-    keptn onboard service --project=catalog --values=./keptn-onboarding/values_front-end.yaml
-    keptn onboard service --project=catalog --values=./keptn-onboarding/values_customer-service.yaml
-    keptn onboard service --project=catalog --values=./keptn-onboarding/values_order-service.yaml
-    keptn onboard service --project=catalog --values=./keptn-onboarding/values_catalog-service.yaml
+    keptn onboard service catalog --project=catalog --chart=./catalog
     
     #keptn add-resource --project=catalog --stage=dev --service=carts --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
     #keptn add-resource --project=catalog --stage=staging --service=carts --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
@@ -19,7 +14,7 @@
     #keptn onboard service carts-db --project=sockshop --chart=./carts-db --deployment-strategy=direct
 
 
-#else 
-#    echo "The helmcharts for catalog are not present"
-#fi 
+else 
+    echo "The helmcharts for catalog are not present"
+fi 
 
