3c1515cd345793fb876a4db4308aeca3cf38f1f7
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-02 18:24:47 -0500

    Update onboard_catalog.sh

--- resources/catalog/onboard_catalog.sh
+++ resources/catalog/onboard_catalog.sh
@@ -3,15 +3,28 @@
 #If directory exists continue, otherwise exit
 if [[ -d "catalog" ]]; then
     # The context for this script needs to be in examples/onboarding-carts
-    keptn create project catalog --shipyard=./shipyard.yaml
+    keptn create project keptnorders --shipyard=./shipyard.yaml
     # Onboarding - prepare  Keptn
-    keptn onboard service catalog-service --project=catalog --chart=./catalog
-    keptn onboard service customer-service --project=catalog --chart=./customer
-    keptn onboard service front-end --project=catalog --chart=./frontend
-    keptn onboard service order-service --project=catalog --chart=./order
+    keptn onboard service catalog --project=keptnorders --chart=./catalog
+    keptn onboard service customer --project=keptnorders --chart=./customer
+    keptn onboard service frontend --project=keptnorders --chart=./frontend
+    keptn onboard service order --project=keptnorders --chart=./order
+
+    keptn add-resource --project=keptnorders --service=frontend --stage=staging --resource=frontend/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=keptnorders --service=frontend --stage=production --resource=frontend/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+
+    keptn add-resource --project=keptnorders --service=order --stage=staging --resource=order/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=keptnorders --service=order --stage=production --resource=order/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+
+    keptn add-resource --project=keptnorders --service=customer --stage=staging --resource=customer/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=keptnorders --service=customer --stage=production --resource=customer/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+
+    keptn add-resource --project=keptnorders --service=catalog --stage=staging --resource=catalog/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=keptnorders --service=catalog --stage=production --resource=catalog/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
+
+    kubectl apply -f dynatrace-sli-config-keptnorders.yaml
     
-    #keptn add-resource --project=catalog --stage=dev --service=front-end --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
-    #keptn add-resource --project=catalog --stage=staging --service=front-end --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
+    
 
 else 
     echo "The helmcharts for catalog are not present"
