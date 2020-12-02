477cb118ee638d55c51397f150e81d66acb59d52
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-02 18:36:04 -0500

    Update onboard_catalog_qualitygates.sh

--- resources/catalog/onboard_catalog_qualitygates.sh
+++ resources/catalog/onboard_catalog_qualitygates.sh
@@ -5,12 +5,12 @@ if [[ -d "catalog" ]]; then
 
     # The context for this script needs to be in examples/onboarding-carts
     echo "Adding the SLI for the Project to all Stages"
-    keptn add-resource --project=catalog --resource=sli-config-dynatrace.yaml --resourceUri=dynatrace/sli.yaml
+    keptn add-resource --project=keptnorders --resource=sli-config-dynatrace.yaml --resourceUri=dynatrace/sli.yaml
 
-    keptn configure monitoring dynatrace --project=catalog
+    keptn configure monitoring dynatrace --project=keptnorders
 
     echo "Setting up QualityGate to Staging"
-    keptn add-resource --project=catalog --stage=staging --service=order-service --resource=slo-quality-gates.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=keptnorders --stage=staging --service=order-service --resource=slo-quality-gates.yaml --resourceUri=slo.yaml
 
 else 
     echo "The helmcharts for catalog are not present"
