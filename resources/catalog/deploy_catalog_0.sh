702bceec5462fa651548ca9232d1e9a4dee4af8c
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-03 15:37:40 -0500

    Update deploy_catalog_0.sh

--- resources/catalog/deploy_catalog_0.sh
+++ resources/catalog/deploy_catalog_0.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
 
 # Trigger the deployment
-#keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
-keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/robjahn/keptn-orders-catalog-service --tag=1
+keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
+#keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/robjahn/keptn-orders-catalog-service --tag=1
 
 # Trigger the deployment
-#keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=1
-keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/robjahn/keptn-orders-order-service --tag=1
+keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=1
+#keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/robjahn/keptn-orders-order-service --tag=1
 
 # Trigger the deployment
-#keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
-keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/robjahn/keptn-orders-front-end --tag=1
+keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
+#keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/robjahn/keptn-orders-front-end --tag=1
 
 # Trigger the deployment
-#keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=1
-keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/robjahn/keptn-orders-customer-service --tag=1
+keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=1
+#keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/robjahn/keptn-orders-customer-service --tag=1
