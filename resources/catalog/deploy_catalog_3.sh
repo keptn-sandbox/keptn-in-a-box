e8aa7bb0d1a1b40cd564d9904900273de82f7970
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-12-03 16:31:48 -0500

    Update deploy_catalog_3.sh

--- resources/catalog/deploy_catalog_3.sh
+++ resources/catalog/deploy_catalog_3.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
-keptn send event new-artifact --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.3
+# Trigger the deployment
+keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
 
+# Trigger the deployment
+keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=3
+
+# Trigger the deployment
+keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
+
+# Trigger the deployment
+keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=3
