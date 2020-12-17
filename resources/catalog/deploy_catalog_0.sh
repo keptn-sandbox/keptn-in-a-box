6493e95475134d222da3d7f35f05a7752ac5d5f7
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2020-12-17 11:07:05 -0500

    updates

--- resources/catalog/deploy_catalog_0.sh
+++ resources/catalog/deploy_catalog_0.sh
@@ -2,16 +2,13 @@
 
 # Trigger the deployment
 keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
-#keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/robjahn/keptn-orders-catalog-service --tag=1
 
 # Trigger the deployment
 keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=1
-#keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/robjahn/keptn-orders-order-service --tag=1
-
-# Trigger the deployment
-keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
-#keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/robjahn/keptn-orders-front-end --tag=1
 
 # Trigger the deployment
 keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=1
-#keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/robjahn/keptn-orders-customer-service --tag=1
+
+# Trigger the deployment
+keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
+
--- resources/catalog/onboard_catalog.sh
+++ resources/catalog/onboard_catalog.sh
@@ -5,10 +5,11 @@ if [[ -d "catalog" ]]; then
     # The context for this script needs to be in examples/onboarding-carts
     keptn create project keptnorders --shipyard=./shipyard.yaml
     # Onboarding - prepare  Keptn
+    keptn onboard service order --project=keptnorders --chart=./order
     keptn onboard service catalog --project=keptnorders --chart=./catalog
     keptn onboard service customer --project=keptnorders --chart=./customer
     keptn onboard service frontend --project=keptnorders --chart=./frontend
-    keptn onboard service order --project=keptnorders --chart=./order
+  
 
     keptn add-resource --project=keptnorders --service=frontend --stage=staging --resource=frontend/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
     keptn add-resource --project=keptnorders --service=frontend --stage=production --resource=frontend/quality-gates/simple_slo.yaml --resourceUri=slo.yaml
