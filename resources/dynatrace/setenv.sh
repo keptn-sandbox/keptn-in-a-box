5c0a7d8c2ab4c6f38b72a036ef79a49bf463425f
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2020-12-22 13:31:30 -0500

    Update setenv.sh

--- resources/dynatrace/setenv.sh
+++ resources/dynatrace/setenv.sh
@@ -4,7 +4,9 @@ source ../dynatrace/utils.sh
 readCredsFromFile
 printVariables
 
+echo $DT_TENANT
+echo $DT_API_TOKEN
+
 export DT_TENANT=$DT_TENANT
 export DT_API_TOKEN=$DT_API_TOKEN
 export DT_PAAS_TOKEN=$DT_PAAS_TOKEN
-
