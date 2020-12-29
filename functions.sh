a943c8c4ea17997b9141e04caaf45f7aeb50d787
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2020-12-29 17:40:04 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -735,11 +735,11 @@ keptndemoCatalogonboard() {
   fi
 }
 
-loadKeptnDashboard()
- if [ "$keptndashboard_load" = true ]; then
+loadKeptnDashboard() {
+  if [ "$keptndashboard_load" = true ]; then
     printInfoSection "Keptn loading Dashboard" 
     bashas "cd $KEPTN_IN_A_BOX_DIR/resources/dynatrace/dashboards && bash load1.sh"
- fi
+  fi
 }
 
 createWorkshopUser() {
