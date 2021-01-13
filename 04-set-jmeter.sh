d81ef2b64c6e653729c2c8cf1ea60dabe7f761fb
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-01-13 09:40:47 -0500

    Update 04-set-jmeter.sh

--- 04-set-jmeter.sh
+++ 04-set-jmeter.sh
@@ -3,4 +3,6 @@
 
 sudo bashas "kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record"
 
+sleep 10
+
 sudo kubectl get pods --all-namespaces | grep jmeter
