681f13cefe5614db12d35a54e9770e9097221fb3
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-02-03 12:16:10 -0500

    release change

--- resetenv.sh
+++ resetenv.sh
@@ -4,7 +4,7 @@
 # Use this script to reset the environment
 #
 KEPTN_IN_A_BOX_REPO="https://github.com/dthotday-performance/keptn-in-a-box.git"
-KIAB_RELEASE="release-0.7.3.1"
+KIAB_RELEASE="release-0.7.3.2"
 KIAB_FILE_REPO="https://raw.githubusercontent.com/dthotday-performance/keptn-in-a-box/${KIAB_RELEASE}/keptn-in-a-box.sh"
 
 sudo snap remove microk8s --purge
