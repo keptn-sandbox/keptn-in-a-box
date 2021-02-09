f601de870bf9f50eeaef27b32258232e978856b4
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-02-09 11:56:00 -0500

    Update 04-set-jmeter.sh

--- 04-set-jmeter.sh
+++ 04-set-jmeter.sh
@@ -3,7 +3,10 @@
 
 ALT_JMETER_SERVICE_BRANCH="release-0.7.3-patch1"
 
-sudo kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
+# 
+#sudo kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
+# use for host aliases 
+sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
 
 sleep 10
 
