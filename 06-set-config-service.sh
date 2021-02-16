0877575d326263064dc6c4ad007615fae3a807e1
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-02-16 07:28:26 -0500

    Update 06-set-config-service.sh

--- 06-set-config-service.sh
+++ 06-set-config-service.sh
@@ -6,6 +6,13 @@ ALT_JMETER_SERVICE_BRANCH="release-0.7.3-patch1"
 # use for host aliases
 sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/configuration-service/deploy/service.yaml -n keptn --record
 
+# use for host aliases
+sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
+
+# use for host aliases
+sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/bridge/deploy/bridge.yaml -n keptn --record
+
+
 sleep 10
 
 sudo kubectl get pods --all-namespaces | grep configuration-service
