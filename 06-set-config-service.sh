1103d37d06ce62617f85c50c6d82c11648d8c3e0
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-03-03 11:18:34 -0500

    Update 06-set-config-service.sh

--- 06-set-config-service.sh
+++ 06-set-config-service.sh
@@ -12,7 +12,7 @@ sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/kep
 # sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/feature/2552/jmeterextensionskeptn072/jmeter-service/deploy/service.yaml -n keptn --record
 
 # use for host aliases
-#sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/bridge/deploy/bridge.yaml -n keptn --record
+sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/bridge/deploy/bridge.yaml -n keptn --record
 
 sleep 10
 
