7d236312b56d5995c302971733b90106bf500728
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-03-02 19:15:31 -0500

    Update gitea-functions.sh

--- resources/gitea/gitea-functions.sh
+++ resources/gitea/gitea-functions.sh
@@ -57,8 +57,12 @@ createKeptnRepos() {
 }
 
 updateKeptnRepo(){
-    KEPTN_PROJECT=$1
+    echo "updating repositories for Keptn projects "
+    for project in `keptn get projects | awk '{ if (NR!=1) print $1}'`;
+    do 
+    KEPTN_PROJECT=$project
     keptn update project $KEPTN_PROJECT --git-user=$GIT_USER --git-token=$API_TOKEN --git-remote-url=$GIT_SERVER/$GIT_USER/$KEPTN_PROJECT.git
+    done
 }
 
 createKeptnRepoManually(){
