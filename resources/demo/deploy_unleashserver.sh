4002ff94e9221a122006972cbeda327e10037684
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2021-10-14 15:51:12 +0200

    fix typo from the tutorial

--- resources/demo/deploy_unleashserver.sh
+++ resources/demo/deploy_unleashserver.sh
@@ -11,12 +11,11 @@ if [[ -d "unleash" ]]; then
     keptn add-resource --project=unleash --service=unleash-db --all-stages --resource=./unleash-db.tgz --resourceUri=helm/unleash-db.tgz
 
     keptn create service unleash --project=unleash
-    keptn add-resource --project=unleash --service=unleash --all-stages --./resource=unleash.tgz --resourceUri=helm/unleash.tgz
-
+    keptn add-resource --project=unleash --service=unleash --all-stages --resource=./unleash.tgz --resourceUri=helm/unleash.tgz
 
     keptn trigger delivery --project=unleash --service=unleash-db --image=postgres:10.4 
-    keptn trigger delivery --project=unleash --service=unleash --image=docker.io/keptnexamples/unleash:1.0.0
 
+    keptn trigger delivery --project=unleash --service=unleash --image=docker.io/keptnexamples/unleash:1.0.0
 
     # Configure Keptn
     kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/unleash-service/release-0.1.0/deploy/service.yaml
